76df461f7bff39bbcd5999f9c2dad6deb50dda3b
Kevin <Kfbritton@gmail.com>
2019-10-18 14:14:57 -0400

    dividend days

--- PyCharm- Midterm.py
+++ PyCharm- Midterm.py
@@ -12,31 +12,59 @@ import re
 # create empty structures
 stockmarket = []  # empty list to be filled with stock tickers
 stock_dict = {}  # empty dict to be filled with stock tickers and dataframe data.
-
-
-
-
+div_days_dict = {}
 # opening paths and creating dictionaries and dataframes
 
 for path in glob.glob('*.csv'): #searches directory for *csv files
     stock_ticker = path.strip('.csv')  # cleans file path of .csv
-    # print(stock_ticker)
     stockmarket.append(stock_ticker) # appends stock names to a list
     stock_dict[stock_ticker] = pd.read_csv(path, usecols=['Date','Close','Adj Close'],index_col='Date') #assigns stock ticker to dictionary key
     stock_dict[stock_ticker].insert(2, 'Close Ratio', stock_dict[stock_ticker]['Close'].shift(+1)/stock_dict[stock_ticker]['Close'])
     stock_dict[stock_ticker].insert(3, 'Adj Close Ratio', stock_dict[stock_ticker]['Adj Close'].shift(+1) / stock_dict[stock_ticker]['Adj Close'])
-    stock_dict[stock_ticker].insert(4, stock_ticker + ' Dividend', abs(round((stock_dict[stock_ticker]['Adj Close Ratio'] - stock_dict[stock_ticker]['Close Ratio']),5)))
+    stock_dict[stock_ticker].insert(4, stock_ticker + ' Dividend',abs(round((stock_dict[stock_ticker]['Adj Close Ratio'] - stock_dict[stock_ticker]['Close Ratio']), 5)))
     universe = pd.concat(stock_dict,axis=1)   # concatenated axis for each stock ticker to a single dataframe
+    div_ser = stock_dict[stock_ticker][stock_ticker + ' Dividend'].loc[stock_dict[stock_ticker][stock_ticker + ' Dividend'] > 0]
+    div_days_dict[stock_ticker] = div_ser
 
 
 
-# print(universe)
+
+# # print(universe)
+# print(div_days_dict.keys())
+# print(div_days_dict)
+
+# print(stock_dict['AAPL']['AAPL Dividend'].loc[stock_dict['AAPL']['AAPL Dividend'] > 0]) # returns values from specific dictonary
+# print(stock_dict['SAP']['SAP Dividend'].loc[stock_dict['SAP']['SAP Dividend'] > 0]) # returns values from specific dictonary
+
+
+# for path in glob.glob('*.csv'): #searches directory for *csv files
+#     stock_ticker = path.strip('.csv')  # cleans file path of .csv
+#     # print(stock_dict[stock_ticker][stock_ticker + ' Dividend'].loc[stock_dict[stock_ticker][stock_ticker + ' Dividend'] > 0])
+#     div_ser = stock_dict[stock_ticker][stock_ticker + ' Dividend'].loc[stock_dict[stock_ticker][stock_ticker + ' Dividend'] > 0]
+#     # print(type(div_ser))
+#     div_days_dict[stock_ticker] = div_ser
+#
+
+# print(universe.to_string())
+
+
+#check types
+# print(type(universe)
+# print(type(stock_dict))
+# print(type(stock_dict['TSLA']))
+# print(stock_dict['TSLA'])
+# print(type(stock_dict['TSLA']['TSLA Dividend']))
+# print(stock_dict['TSLA']['TSLA Dividend'])
+# print(stock_dict['TSLA']['TSLA Dividend'].to_string())
+# print(dfTSLA.loc[(stock_dict['TSLA']['TSLA Dividend']>0)])
+
+
+
+
 
 # print(universe['TSLA']['TSLA Dividend'].loc[universe['TSLA']['TSLA Dividend']>0]) # Cannot index with multidimensional key
-# print(stock_dict['AAPL']['AAPL Dividend'].loc[stock_dict['AAPL']['AAPL Dividend'] > 0]) # returns values from specific dictonary
-
-print(stock_dict['TSLA']['TSLA Dividend'].loc[stock_dict['TSLA']['TSLA Dividend'] > 0]) # returns values from specific dictonary
-print(stock_dict['TSLA']['TSLA Dividend'].loc[stock_dict['TSLA']]) #https://github.com/pandas-dev/pandas/issues/7981 , ValueError: Cannot index with multidimensional key
+# print(stock_dict['TSLA']['TSLA Dividend'].loc[stock_dict['TSLA']['TSLA Dividend'] > 0]) # returns values from specific dictonary
+# print(stock_dict['TSLA']['TSLA Dividend'].loc[stock_dict['TSLA']]) #https://github.com/pandas-dev/pandas/issues/7981 , ValueError: Cannot index with multidimensional key
 
 
 
